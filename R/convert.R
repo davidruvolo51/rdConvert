175896dc24204dabb89f5b822d24f94bba887395
davidruvolo51 <davidruvolo51@gmail.com>
2020-10-17 23:39:04 +0200

    added basic markdown formatters

--- R/convert.R
+++ R/convert.R
@@ -25,45 +25,55 @@ convert <- R6::R6Class(
     "rd_converter",
     public = list(
 
-        #' @field entry path to Rd files
-        entry = "man",
+        #' @field entry_dir path to Rd files
+        entry_dir = "man",
 
-        #' @field destDir output directory
-        destDir = NA,
+        #' @field dest_dir output directory
+        dest_dir = NA,
 
-        #' @field files list of entry files and output paths
-        files = NA,
+        #' @field entries list of entry files and output paths
+        entries = NA,
+
+        #' @field file_format save file as md or mdx
+        file_format = "md",
 
         #' @description Create a new converter
-        #' @param destDir output directory for markdown files
-        initialize = function(destDir = "src/pages") {
-            self$destDir <- format_path(destDir)
+        #' @param dest_dir output directory for markdown files
+        #' @param file_format save file as md or mdx
+        initialize = function(dest_dir = "src/pages", file_format = "md") {
+            self$dest_dir <- format_path(dest_dir)
+
+            formats <- c("md", "mdx")
+            if (file_format %in% formats) {
+                self$file_format <- file_format
+            } else {
+                cli::cli_alert_danger(
+                    "{.val {file_format} is invalid. Use {.val {formats}}}"
+                )
+            }
         },
 
         #' @description Set entry points and output file paths
         set_entries = function() {
-            self$files <- list.files(
-                path = self$entry,
+            self$entries <- list.files(
+                path = self$entry_dir,
                 full.names = TRUE
             ) %>%
                 as.data.frame(.) %>%
                 mutate(
                     name = format_rd_path(.),
-                    outDir = paste0(
-                        self$destDir, "/",
-                        format_rd_path(.)
-                    ),
+                    outDir = paste0(self$dest_dir, "/"),
                     outFile = paste0(
-                        self$destDir, "/",
-                        format_rd_path(.),
-                        "/index.md"
+                        self$dest_dir, "/",
+                        format_rd_path(.), ".",
+                        self$file_format
                     )
                 ) %>%
                 select(., name, entry = ., outDir, outFile)
 
-            if (!is.null(self$files)) {
+            if (NROW(self$entries) > 0) {
                 cli::cli_alert_success(
-                    "Entry and destination points collated. See {.arg $files}"
+                    "Entry and destination points collated. See {.var $entries}"
                 )
             } else {
                 cli::cli_alert_danger(
@@ -74,51 +84,65 @@ convert <- R6::R6Class(
 
         #' @description set destinitions
         set_destinations = function() {
-            create_destinations(self$destDir)
+            create_destinations(self$dest_dir)
         },
 
         #' @description batch convert Rd files to markdown files with YAML
         convert_rds = function() {
-            for (d in seq_len(NROW(self$files))) {
-                tmp_entry <- self$files$entry[d]
-                tmp_dir <- self$files$outDir[d]
-                tmp_out <- self$files$outFile[d]
+            for (d in seq_len(NROW(self$entries))) {
+                tmp_name <- basename(self$entries$outFile[d])
+                tmp_entry <- self$entries$entry[d]
+                tmp_dir <- self$entries$outDir[d]
+                tmp_out <- self$entries$outFile[d]
 
-                if (!dir.exists(tmp_dir)) dir.create(tmp_dir)
-                file.create(tmp_out)
-                cli::cli_alert_success(
-                    text = "Rendered {.val index.md} at {.val {tmp_dir}}"
-                )
-                Rd2md::Rd2markdown(tmp_entry, tmp_out, append = FALSE)
+                tryCatch({
+                    Rd2md::Rd2markdown(tmp_entry, tmp_out, append = FALSE)
+                    cli::cli_alert_success(
+                        "Rendered {.file {tmp_name}} at {.path {tmp_dir}}"
+                    )
+                }, warning = function(warn) {
+                    cli::cli_alert_danger("Error in {.fun convert_rds}: {warn}")
+                }, error = function(err) {
+                    cli::cli_alert_danger("Error in {.fun convert_rds}: {err}")
+                })
             }
         },
 
         #' @description inject YAML from Rd files into rendered markdown files
         add_yaml = function() {
-            for (d in seq_len(NROW(self$files))) {
-                tmp_entry <- self$files$entry[d]
-                tmp_out <- self$files$outFile[d]
+            for (d in seq_len(NROW(self$entries))) {
+                tmp_name <- basename(self$entries$outFile[d])
+                tmp_entry <- self$entries$entry[d]
+                tmp_out <- self$entries$outFile[d]
 
-                rd <- Rd2roxygen::parse_file(tmp_entry)
-                yaml <- c(
-                    "---",
-                    paste0("title: ", format_yaml_text(rd$title)),
-                    paste0("subtitle: ", format_yaml_text(rd$value)),
-                    paste0("date: ", dQuote(Sys.Date())),
-                    paste0(
-                        "keywords: ",
-                        format_yaml_keywords(rd$keywords)
-                    ),
-                    "---",
-                    "\n"
-                )
+                tryCatch({
+                    rd <- Rd2roxygen::parse_file(tmp_entry)
+                    yaml <- c(
+                        "---",
+                        paste0("title: ", format_yaml_text(rd$title)),
+                        paste0("description: ", format_yaml_text(rd$value)),
+                        paste0("date: ", dQuote(Sys.Date())),
+                        paste0(
+                            "keywords: ",
+                            format_yaml_keywords(rd$keywords)
+                        ),
+                        "---",
+                        ""
+                    )
 
-                md <- readLines(tmp_out, warn = FALSE)
-                writeLines(c(yaml, md), tmp_out)
+                    md <- readLines(tmp_out, warn = FALSE) %>%
+                        format_md_table() %>%
+                        format_leading_whitespace(.) %>%
+                        format_blank_lines(.)
+                    writeLines(c(yaml, md), tmp_out)
 
-                cli::cli_alert_success(
-                    text = "Added yaml to {.val index.md} at {.val {tmp_out}}"
-                )
+                    cli::cli_alert_success("Added yaml to {.file {tmp_name}}")
+
+                }, warning = function(warn) {
+                    cli::cli_alert_danger("Error in {.fun add_yaml}: {warn}")
+                }, error = function(err) {
+                    cli::cli_alert_danger("Error in {.fun add_yaml}: {err}")
+                })
             }
         }
     )
--- R/format.R
+++ R/format.R
@@ -63,3 +63,90 @@ format_yaml_text <- function(x) {
         ""
     }
 }
+
+
+#' Format Leading Whitespace
+#'
+#' In many text blocks, there a leading white space is added. Remove it!
+#'
+#' @param x a string from an md file
+#'
+#' @export
+format_leading_whitespace <- function(x) {
+    sapply(
+        seq_len(length(x)),
+        function(d) {
+            trimws(x[d], "both")
+        }
+    )
+}
+
+
+#' Format sequential blank lines
+#'
+#' If there are two blank lines in a row, remove the second item
+#'
+#' @param x a character
+#'
+#' @export
+format_blank_lines <- function(x) {
+    counter <- 0
+    rm_indices <- c()
+    sapply(
+        seq_len(length(x)),
+        function(n) {
+            if (x[n] == "") {
+                counter <<- counter + 1
+                if (counter > 1) {
+                    rm_indices <<- c(rm_indices, n)
+                }
+            } else {
+                counter <<- 0
+            }
+        }
+    )
+    x[-rm_indices]
+}
+
+#' Format Markdown Table
+#'
+#' Fix markdown table headers, table alignments, and argument cells.
+#' Remove extra whitespacing and multiple backticks. Enclose rows in
+#' with a vertical line.
+#'
+#' @param x a string or array containing markdown
+#'
+#' @export
+format_md_table <- function(x) {
+    sapply(
+        seq_len(length(x)),
+        function(d) {
+
+            # adjust markdown table headers
+            if (grepl("^Argument\\s+\\|Description$", x[d])) {
+                gsub(
+                    pattern = "^Argument\\s+\\|(\\s+)?Description$",
+                    replacement = "| Argument | Description |",
+                    x = x[d]
+                )
+            } else if (grepl("^[-]+(\\s+?)\\|[-]+$", x[d])) {
+
+                # fix markdown table alignment
+                gsub(
+                    pattern = "^[-]+(\\s+?)\\|[-]+$",
+                    replacement = "| -------- | ----------- |",
+                    x = x[d]
+                )
+            } else if (grepl("^[`]{3}\\w+[`]{3}(\\s+?)\\|(\\s+?)\\w+", x[d])) {
+
+                # fix table row columns
+                a <- paste0("| ", x[d], " |")
+                b <- gsub("[`]{3}", "`", a)
+                gsub("\\s+", " ",  b)
+
+            } else {
+                x[d]
+            }
+        }
+    )
+}

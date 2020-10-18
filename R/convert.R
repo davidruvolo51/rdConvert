eb1acfdfa07a150ba2c40c75e3df2813be0e7fef
davidruvolo51 <davidruvolo51@gmail.com>
2020-10-18 21:56:45 +0200

    added methods for working with rocket-docs

--- R/convert.R
+++ R/convert.R
@@ -24,6 +24,8 @@
 convert <- R6::R6Class(
     "rd_converter",
     public = list(
+        #' @field pkg_name project name
+        pkg_name = NA,
 
         #' @field entry_dir path to Rd files
         entry_dir = "man",
@@ -37,10 +39,27 @@ convert <- R6::R6Class(
         #' @field file_format save file as md or mdx
         file_format = "md",
 
+        #' @field yml configuration file for sidebar.yml
+        yml = NA,
+
+        #' @field repo_url address to github repo
+        repo_url = NA,
+
         #' @description Create a new converter
+        #' @param pkg_name name of the project
         #' @param dest_dir output directory for markdown files
         #' @param file_format save file as md or mdx
-        initialize = function(dest_dir = "src/pages", file_format = "md") {
+        #' @param repo_url address to github repo
+        initialize = function(
+            pkg_name,
+            dest_dir = "src/docs/usage",
+            file_format = "md",
+            repo_url = NULL
+        ) {
+            if (is.null(pkg_name)) {
+                cli::cli_alert_danger("{.arg pkg_name} cannot be missing")
+            }
+            self$pkg_name <- pkg_name
             self$dest_dir <- format_path(dest_dir)
 
             formats <- c("md", "mdx")
@@ -51,11 +70,15 @@ convert <- R6::R6Class(
                     "{.val {file_format} is invalid. Use {.val {formats}}}"
                 )
             }
+
+            if (!is.null(repo_url)) {
+                self$repo_url <- repo_url
+            }
         },
 
         #' @description Set entry points and output file paths
         set_entries = function() {
-            self$entries <- list.files(
+            f <- list.files(
                 path = self$entry_dir,
                 full.names = TRUE
             ) %>%
@@ -70,6 +93,17 @@ convert <- R6::R6Class(
                     )
                 ) %>%
                 select(., name, entry = ., outDir, outFile)
+
+            # add data yml config
+            self$entries <- f %>%
+                mutate(
+                    yml_label = gsub("_", " ", name) %>% tools::toTitleCase(.),
+                    yml_link = paste0(
+                        basename(outDir), "/",
+                        gsub(".md|.mdx", "", basename(outFile))
+                    )
+                )
+
 
             if (NROW(self$entries) > 0) {
                 cli::cli_alert_success(
@@ -108,6 +142,32 @@ convert <- R6::R6Class(
             }
         },
 
+        #' @description format markdown files
+        format_markdown = function() {
+            for (d in seq_len(NROW(self$entries))) {
+                tmp_name <- basename(self$entries$outFile[d])
+                tmp_entry <- self$entries$entry[d]
+                tmp_out <- self$entries$outFile[d]
+
+                tryCatch({
+                    md <- readLines(tmp_out, warn = FALSE) %>%
+                        format_md_table() %>%
+                        format_leading_whitespace(.) %>%
+                        format_blank_lines(.)
+                    writeLines(md, tmp_out)
+                    cli::cli_alert_success("Formatted {.file {tmp_name}}")
+                }, warning = function(warn) {
+                    cli::cli_alert_danger(
+                        "Error in {.fun format_markdown}: {warn}"
+                    )
+                }, error = function(err) {
+                    cli::cli_alert_danger(
+                        "Error in {.fun format_markdown}: {err}"
+                    )
+                })
+            }
+        },
+
         #' @description inject YAML from Rd files into rendered markdown files
         add_yaml = function() {
             for (d in seq_len(NROW(self$entries))) {
@@ -129,21 +189,83 @@ convert <- R6::R6Class(
                         "---",
                         ""
                     )
-
-                    md <- readLines(tmp_out, warn = FALSE) %>%
-                        format_md_table() %>%
-                        format_leading_whitespace(.) %>%
-                        format_blank_lines(.)
+                    md <- readLines(tmp_out, warn = FALSE)
                     writeLines(c(yaml, md), tmp_out)
-
                     cli::cli_alert_success("Added yaml to {.file {tmp_name}}")
-
                 }, warning = function(warn) {
                     cli::cli_alert_danger("Error in {.fun add_yaml}: {warn}")
                 }, error = function(err) {
                     cli::cli_alert_danger("Error in {.fun add_yaml}: {err}")
                 })
             }
+        },
+
+        #' @description generate sidebar yml
+        #' @param home display home link in sidebar
+        #' @param repo_url address to github repo
+        set_sidebar_yml = function(home = TRUE, repo_url = NULL) {
+            config <- c("# Sidebar navigation", "")
+
+            # include home link using pkg_name
+            if (home) {
+                config <- c(
+                    config,
+                    paste0("- label: '", self$pkg_name, "'"),
+                    "    link: '/'"
+                )
+            }
+
+            # generate function links
+            links <- sapply(
+                seq_len(NROW(self$entries)),
+                function(d) {
+                    c(
+                        paste0(
+                            "    - label: '",
+                            self$entries$yml_label[d],
+                            "'"
+                        ),
+                        paste0(
+                            "      link: '/",
+                            self$entries$yml_link[d],
+                            "'"
+                        )
+                    )
+                }
+            )
+
+            config <- c(
+                config,
+                "- label: Reference",
+                "  items:",
+                as.character(links)
+            )
+
+            if (!is.null(repo_url)) {
+                config <- c(
+                    config,
+                    "-label: Community",
+                    "  items:",
+                    "    - label: 'Github'",
+                    paste0("      link: '", repo_url, "'")
+                )
+            }
+
+            self$yml <- config
+            cli::cli_alert_success("Created sidebar yml. See {.arg $yml}")
+        },
+
+        #' @description write yml config
+        #' @param path path for configuration file
+        save_sidebar_yml = function(path) {
+            tryCatch({
+                writeLines(self$yml, path)
+                cli::cli_alert_success("Saved {.file config/sidebar.yml}")
+            }, error = function(e) {
+                cli::cli_alert_danger("Error: {e}")
+            }, warning = function(w) {
+                cli::cli_alert_danger("Warning: {w}")
+            })
         }
     )
 )
